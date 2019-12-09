6a02abb24daafd71dfb57137a2110e2faf413d4f
Charles Pelton <charobpel@gmail.com>
2019-12-09 06:11:07 -0800

    Requires auto-increment triggers serverside for adding/deleting inventory/characters

--- Borderlands2019GUI.py
+++ Borderlands2019GUI.py
@@ -9,6 +9,7 @@ global e2
 global e
 global user
 global characters
+global main
 #connection to DB
 global connection
 global cursor
@@ -51,10 +52,34 @@ global ammo4
 global ammo5
 global ammo6
 global ammo7
+#variables to create
+global u
+global a
+global v
+#insert
 inventory = ("INSERT INTO inventory "
              "(user_id, hunter_id, gun_id, shield, class_mod, relic, grenade_mod) "
              "VALUES (%s,%s,%s,%s,%s,%s,%s)"
              )
+charInsert = ("INSERT INTO vault_hunter "
+             "(user_id, class, level, name)"
+             "VALUES (%s,%s,%s,%s)"
+             )
+#additional ui
+global confirm
+global confirm2
+global k1
+global k2
+global k3
+global k4
+def errorbox(title, content):
+    e = tkinter.Tk()
+    e.minsize(260, 40)
+    e.title(title)
+    tkinter.Label(e, text=content).pack()
+    button = tkinter.Button(e, text='Okay', width=25, command=e.destroy)
+    button.pack()
+
 def query():
     global listbox
     global previousSearch
@@ -67,7 +92,6 @@ def query():
     global gunShotgun
     global gunSMG
     global gunSniper
-    global var1
     global list1
     global var2
     global list2
@@ -391,9 +415,10 @@ def onSelectCharacter(event=None):
     cursor.execute("SELECT * from vault_hunter join user on vault_hunter.user_id = user.user_id where username = \'" +
                    e1 + "\' and name = \'" + previousCharacter + "\'")
     character = cursor.fetchall()
-    cursor.execute("SELECT * from currency where hunter_id = \'" + str(character[0][0])+ "\'")
+    cursor.execute("SELECT * from currency where hunter_id = \'" + str(character[0][0])+ "\' and user_id = \'" +
+                   str(character[0][1]) + "\'")
     character = cursor.fetchall()
-    for(id, mon, erid, pis, snip, shot, rif, sub, roc, gren, key) in character:
+    for(id, mon, erid, pis, snip, shot, rif, sub, roc, gren, key, uid) in character:
         money.delete(0, tkinter.END)
         money.insert(tkinter.END, mon)
         eridium.delete(0, tkinter.END)
@@ -423,7 +448,6 @@ def addtochar():
     global gunShotgun
     global gunSMG
     global gunSniper
-    global var1
     global list1
     global var2
     global list2
@@ -605,14 +629,16 @@ def addtochar():
                     else:
                         print(row)
                         cursor.execute(inventory, (character[0][1], character[0][0], None, None, row[0], None, None))
-                    e = tkinter.Tk()
-                    e.minsize(260, 40)
-                    e.title('Added Item')
-                    tkinter.Label(e, text=row[1]+" added to "+previousCharacter).pack()
-                    button = tkinter.Button(e, text='Okay', width=25, command=e.destroy)
-                    button.pack()
+                    errorbox('Added Item', (row[1] +" added to "+ previousCharacter))
                     connection.commit()
                     query()
+            else:
+                errorbox('Cannot Add Item', "Please select a character to add to")
+        else:
+            errorbox('Cannot Add Item', "No query or item not selected")
+    else:
+        errorbox('Cannot Add Item', "Incorrect Search: Try Show All Items or Show Character Inventory")
+
 def deletefromchar():
     global cursor
     global connection
@@ -768,17 +794,125 @@ def deletefromchar():
                     cursor.execute("delete from inventory where relic = \'" + str(row[0]) + "\' and user_id = \'" +
                                    str(character[0][1]) + "\' and hunter_id = \'" + str(character[0][0]) + "\' LIMIT 1")
                 else:
-                    print(row)
                     cursor.execute("delete from inventory where class_mod = \'" + str(row[0]) + "\' and user_id = \'" +
                                    str(character[0][1]) + "\' and hunter_id = \'" + str(character[0][0]) + "\' LIMIT 1")
-                e = tkinter.Tk()
-                e.minsize(260, 40)
-                e.title('Deleted Item')
-                tkinter.Label(e, text=row[1] + " deleted from " + previousCharacter).pack()
-                button = tkinter.Button(e, text='Okay', width=25, command=e.destroy)
-                button.pack()
+                errorbox('Deleted Item', (row[1] + " deleted from " + previousCharacter))
                 connection.commit()
                 query()
+            else:
+                errorbox('Cannot Delete Item', "No query: please check an item")
+        else:
+            errorbox('Cannot Delete Item', "No query or item not selected")
+    else:
+        errorbox('Cannot Delete Item', "Show your inventory with a character to delete an item")
+
+def cChar():
+    global cursor
+    global connection
+    global charInsert
+    global previousCharacter
+    global characters
+    global main
+    global k1
+    global u
+    global a
+    global v
+    if u.get() != '':
+        if v.get() != '. . .':
+            if a.get() == '':
+                a = 1
+                cursor.execute(charInsert, (user[0][0], v.get(), a, u.get()))
+                connection.commit()
+                cursor.execute("SELECT * from vault_hunter where user_id = \'" + str(user[0][0]) + "\'")
+                characters = cursor.fetchall()
+                names = []
+                for (hid, cid, cass, level, name) in characters:
+                    names.append(name)
+                combochar = ttk.Combobox(main, width=30, values=names)
+                combochar.grid(row=4, column=0, columnspan=3, padx=30)
+                combochar.set(". . .")
+                previousCharacter = ". . ."
+                combochar.bind('<<ComboboxSelected>>', onSelectCharacter)
+                errorbox('Created Character', (u.get() + " created!"))
+            elif a.get() != '':
+                try:
+                    if 0 < int(a.get()) <= 100:
+                        a = int(a.get())
+                        cursor.execute(charInsert, (user[0][0], v.get(), a, u.get()))
+                        connection.commit()
+                        cursor.execute("SELECT * from vault_hunter where user_id = \'" + str(user[0][0]) + "\'")
+                        characters = cursor.fetchall()
+                        names = []
+                        for (hid, cid, cass, level, name) in characters:
+                            names.append(name)
+                        combochar = ttk.Combobox(main, width=30, values=names)
+                        combochar.grid(row=4, column=0, columnspan=3, padx=30)
+                        combochar.set(". . .")
+                        previousCharacter = ". . ."
+                        combochar.bind('<<ComboboxSelected>>', onSelectCharacter)
+                        errorbox('Created Character', (u.get() + " created!"))
+                except:
+                    errorbox('Cannot Create Character', "Invalid Level, try a value between 1 and 100")
+            else:
+                errorbox('Cannot Create Character', "Invalid Level, try a value between 1 and 100")
+        else:
+            errorbox('Cannot Create Character', "Please select a class")
+    else:
+        errorbox('Cannot Create Character', "Invalid name")
+    k1.destroy()
+
+def createChar():
+    global k1
+    global u
+    global a
+    global v
+    k1 = tkinter.Tk()
+    k1.title('New Character')
+    k1.minsize(300, 50)
+    tkinter.Label(k1, text='Name:').grid(row=0, column=0)
+    u = tkinter.Entry(k1)
+    u.grid(row=0, column=1)
+    tkinter.Label(k1, text='Level(Defaults to 1):').grid(row=1, column=0)
+    a = tkinter.Entry(k1)
+    a.grid(row=1, column=1)
+    tkinter.Label(k1, text='Class:').grid(row=2, column=0)
+    v = ttk.Combobox(k1, width=20, values=("Siren", "Commando", "Gunzerker", "Assassin", "Mechromancer", "Psycho"))
+    v.set(". . .")
+    v.grid(row=2, column=1)
+    tkinter.Button(k1, text='Create', width=10, command=cChar).grid(row=3, column=1)
+
+def uChar():
+    global cursor
+    global connection
+    global previousCharacter
+    global previousSearch
+    global e1
+    global k2
+    global v
+    cursor.execute("SELECT * from vault_hunter join user on vault_hunter.user_id = user.user_id where "
+    "username = \'" + e1 + "\' and name = \'" + previousCharacter + "\'")
+    character = cursor.fetchall()
+    cursor.execute("UPDATE vault_hunter set class = \'"+ v.get() +"\' where user_id = \'" +
+    str(character[0][1]) +"\' and hunter_id = \'"+ str(character[0][0]) +"\'")
+    connection.commit()
+    k2.destroy()
+    if previousSearch == "Show Current Character Stats and Badass":
+        query()
+
+def updateChar():
+    if previousCharacter != '. . .':
+        global k2
+        global v
+        k2 = tkinter.Tk()
+        k2.title('Update Class')
+        k2.minsize(300, 50)
+        tkinter.Label(k2, text='Class:').grid(row=0, column=0)
+        v = ttk.Combobox(k2, width=20, values=("Siren", "Commando", "Gunzerker", "Assassin", "Mechromancer", "Psycho"))
+        v.set(". . .")
+        v.grid(row=0, column=1)
+        tkinter.Button(k2, text='Update', width=10, command=uChar).grid(row=1, column=1)
+    else:
+        errorbox('Error', "No character selected for class update")
 def update():
     global e1
     global cursor
@@ -798,19 +932,79 @@ def update():
         cursor.execute("Update currency set skeletonkeys = " + str(keys.get()) + " where hunter_id = \'"
                        + str(character[0][0]) + "\'")
         connection.commit()
-        e = tkinter.Tk()
-        e.minsize(260, 40)
-        e.title('Update Successful')
-        tkinter.Label(e, text=previousCharacter + " currency updated!").pack()
-        button = tkinter.Button(e, text='Okay', width=25, command=e.destroy)
-        button.pack()
+        errorbox('Update Successful', (previousCharacter + " currency updated!"))
     else:
-        e = tkinter.Tk()
-        e.minsize(260, 40)
-        e.title('Error')
-        tkinter.Label(e, text='No character selected for currency update').pack()
-        button = tkinter.Button(e, text='Okay', width=25, command=e.destroy)
-        button.pack()
+        errorbox('Error', 'No character selected for currency update')
+
+def dChar():
+    global confirm
+    global cursor
+    global connection
+    global previousCharacter
+    global e1
+    confirm.destroy()
+    if previousCharacter != '. . .':
+        cursor.execute(
+            "SELECT hunter_id, vault_hunter.user_id from vault_hunter join user on vault_hunter.user_id = user.user_id where "
+            "username = \'" + e1 + "\' and name = \'" + previousCharacter + "\'")
+        character = cursor.fetchall()
+        cursor.execute("Delete from vault_hunter where user_id = " + str(character[0][1]) + " and hunter_id = " + str(character[0][0]))
+        connection.commit()
+        errorbox('Character Deleted', (previousCharacter + " deleted!"))
+        cursor.execute("SELECT * from vault_hunter where user_id = \'" + str(user[0][0]) + "\'")
+        characters = cursor.fetchall()
+        names = []
+        for (hid, cid, cass, level, name) in characters:
+            names.append(name)
+        combochar = ttk.Combobox(main, width=30, values=names)
+        combochar.grid(row=4, column=0, columnspan=3, padx=30)
+        combochar.set(". . .")
+        previousCharacter = ". . ."
+        combochar.bind('<<ComboboxSelected>>', onSelectCharacter)
+    else:
+        errorbox('Error', 'No character selected to delete')
+
+def deleteChar():
+    global previousCharacter
+    if previousCharacter != '. . .':
+        global confirm
+        confirm = tkinter.Tk()
+        confirm.title('Delete Character')
+        tkinter.Label(confirm, text='ARE you sure you want to delete your current selected character?').grid(row=0)
+        tkinter.Button(confirm, text='Yes', width=25, command=dChar).grid(row=1, column=0)
+        tkinter.Button(confirm, text='No', width=25, command=confirm.destroy).grid(row=1, column=1)
+    else:
+        errorbox('Error', 'No character selected to delete')
+
+def dAll():
+    global confirm2
+    global cursor
+    global connection
+    global previousCharacter
+    global e1
+    global main
+    confirm2.destroy()
+    cursor.execute(
+        "SELECT hunter_id, vault_hunter.user_id from vault_hunter join user on vault_hunter.user_id = user.user_id where "
+        "username = \'" + e1 + "\'")
+    character = cursor.fetchall()
+    cursor.execute("Delete from vault_hunter where user_id = " + str(character[0][1]))
+    connection.commit()
+    main.destroy()
+    start()
+    errorbox('Account Deleted', "Please log in again to create a new account!")
+
+def deleteAll():
+    global previousCharacter
+    if previousCharacter != '. . .':
+        global confirm2
+        confirm2 = tkinter.Tk()
+        confirm2.title('Delete User')
+        tkinter.Label(confirm2, text='ARE you sure you want to delete your user? All Data will be lost.').grid(row=0)
+        tkinter.Button(confirm2, text='Yes', width=25, command=dAll).grid(row=1, column=0)
+        tkinter.Button(confirm2, text='No', width=25, command=confirm2.destroy).grid(row=1, column=1)
+    else:
+        errorbox('Error', 'No character selected to delete')
 
 def initalize():
     global r
@@ -819,6 +1013,7 @@ def initalize():
     global characters
     global connection
     global cursor
+    global main
     global listbox
     global previousSearch
     global previousCharacter
@@ -871,7 +1066,7 @@ def initalize():
     #search commands '. . .' negates searches
     tkinter.Label(main, text='Search', relief=tkinter.RIDGE).grid(row=0, column=0)
     combo = ttk.Combobox(main, width=100, values=("Show Character Inventory", "Show Current Character Stats and Badass",
-     "Show All Items(Ignore Checkboxes)", "Show All Items", "Show All Item Locations"))
+     "Show All Items(Ignore Checkboxes)", "Show All Items", "Show All Item Locations", "Show All Vehicles"))
     combo.set(". . .")
     combo.grid(row=1, columnspan=10, padx=30)
     combo.bind('<<ComboboxSelected>>', onSelectSearch)
@@ -912,14 +1107,23 @@ def initalize():
     keys = tkinter.Entry(main)
     keys.grid(row=7, column=1, sticky="W")
     updateb = tkinter.Button(main, text='Update Currency', command=update)
-    updateb.grid(row=8, column=0, columnspan=2)
+    updateb.grid(row=8, column=1, sticky="W")
+    tkinter.Label(main, width=10).grid(row=9, column=0)
+    createc = tkinter.Button(main, text='Create Character', command=createChar)
+    createc.grid(row=10, column=0, padx=10)
+    deletec = tkinter.Button(main, text='Delete Character', command=deleteChar)
+    deletec.grid(row=10, column=1)
+    updatec = tkinter.Button(main, text='Update Class', command=updateChar)
+    updatec.grid(row=10, column=2, sticky="W")
+    deleteu = tkinter.Button(main, text='Delete User', command=deleteAll)
+    deleteu.grid(row=10, column=9, sticky="W")
     #list of checkboxes for scope of search
     gunAR = tkinter.IntVar()
     tkinter.Checkbutton(main, text="Assault Rifle", variable=gunAR, anchor="w", command=query).\
         grid(row=4, column=3, columnspan=3, sticky="W")
     gunSMG = tkinter.IntVar()
     tkinter.Checkbutton(main, text="Submachine Gun", variable=gunSMG, anchor="w", command=query). \
-        grid(row=5, column=3, columnspan=4, sticky="W")
+        grid(row=5, column=3, columnspan=5, sticky="W")
     gunShotgun = tkinter.IntVar()
     tkinter.Checkbutton(main, text="Shotgun", variable=gunShotgun, anchor="w", command=query). \
         grid(row=6, column=3, columnspan=3, sticky="W")
@@ -931,7 +1135,7 @@ def initalize():
         grid(row=8, column=3, columnspan=3, sticky="W")
     gunRocket = tkinter.IntVar()
     tkinter.Checkbutton(main, text="Rocket Launcher", variable=gunRocket, anchor="w", command=query). \
-        grid(row=9, column=3, columnspan=3, sticky="W")
+        grid(row=9, column=3, columnspan=5, sticky="W")
     #other items
     var2 = tkinter.IntVar()
     tkinter.Checkbutton(main, text="Shields", variable=var2, anchor="w", command=query).\
@@ -946,18 +1150,18 @@ def initalize():
     tkinter.Checkbutton(main, text="Relics", variable=var5, anchor="w", command=query).\
         grid(row=7, column=8, columnspan=3, sticky="W")
     #sets up listbox and mousewheel scroll capabilities
-    tkinter.Label(main, width=1).grid(row=9)
-    tkinter.Label(main, text='Results', relief=tkinter.RIDGE).grid(row=10)
+    tkinter.Label(main, width=1).grid(row=11)
+    tkinter.Label(main, text='Results', relief=tkinter.RIDGE).grid(row=12)
     scrollbar = tkinter.Scrollbar(main)
     listbox = tkinter.Listbox(main, width=100, yscrollcommand=scrollbar.set)
     listbox.insert(tkinter.END, ". . .")
-    listbox.grid(row=11, columnspan=10, rowspan=10, padx=30)
+    listbox.grid(row=13, columnspan=10, rowspan=10, padx=30)
     scrollbar.config(command=listbox.yview)
     #button for list, add and destroy
     addb = tkinter.Button(main, text='Add/Duplicate to Character', command=addtochar)
-    addb.grid(row=21, column=1)
-    deleteb = tkinter.Button(main, text='Delete from Character', command=deletefromchar)
-    deleteb.grid(row=21, column=7)
+    addb.grid(row=23, column=1)
+    deleteb = tkinter.Button(main, text='Delete item from character', command=deletefromchar)
+    deleteb.grid(row=23, column=7)
     main.mainloop()
 
 def login():
@@ -976,26 +1180,11 @@ def login():
         auth_plugin='mysql_native_password')
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            e = tkinter.Tk()
-            e.minsize(260, 40)
-            e.title('Error')
-            tkinter.Label(e, text='user name or password is incorrect').pack()
-            button = tkinter.Button(e, text='Okay', width=25, command=e.destroy)
-            button.pack()
+            errorbox('Error', 'user name or password is incorrect')
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            e = tkinter.Tk()
-            e.minsize(260, 40)
-            e.title('Error')
-            tkinter.Label(e, text=err).pack()
-            button = tkinter.Button(e, text='database does not exist', width=25, command=e.destroy)
-            button.pack()
+            errorbox('Error', 'database does not exist')
         else:
-            e = tkinter.Tk()
-            e.minsize(260, 40)
-            e.title('Error')
-            tkinter.Label(e, text=err).pack()
-            button = tkinter.Button(e, text='Okay', width=25, command=e.destroy)
-            button.pack()
+            errorbox('Error', err)
     else:
         initalize()
 
